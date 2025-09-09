00f24ba72da88256d101e7ab8e7b933139ca6d09
jpmaciel22 <jpbmaciel@gmail.com>
2025-09-09 17:27:17 -0300

    debugzinhe

---
+++ Lexer-Parser-07/debug/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ocelot/Compiladores/Lexer-Parser-07/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/home/ocelot/Compiladores/Lexer-Parser-07/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/home/ocelot/Compiladores/Lexer-Parser-07/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/home/ocelot/Compiladores/Lexer-Parser-07/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lexer-Parser-07/debug/CMakeFiles/tradutor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tradutor.dir/error.cpp.o"
+  "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "CMakeFiles/tradutor.dir/lexer.cpp.o"
+  "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "CMakeFiles/tradutor.dir/parser.cpp.o"
+  "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "CMakeFiles/tradutor.dir/tradutor.cpp.o"
+  "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  "tradutor"
+  "tradutor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tradutor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
