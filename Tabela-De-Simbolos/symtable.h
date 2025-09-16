4acec2ea65a94a15ac4fc9bf39e4b2f2043e9133
jpmaciel22 <jpbmaciel@gmail.com>
2025-09-16 17:35:06 -0300

    ...

--- Tabela-De-Simbolos/symtable.h
+++ Tabela-De-Simbolos/symtable.h
@@ -3,21 +3,26 @@
 using std::unordered_map;
 using std::string;
 
-//modelo dos simbolos
-struct Symbol{
-    string var; 
-    string type;
+
+// modelo para símbolos
+struct Symbol
+{
+	string var;
+	string type;
 };
 
-//tabela de simbolos
-class SymTable{
-    private: 
-        unordered_map<string,Symbol> table;
-        SymTable * prev;
-    public:
-        SymTable();
-        SymTable(SymTable * t);
 
-        bool Insert(string s, Symbol symb);
-        Symbol * Find(string s);
+// tabela de símbolos
+class SymTable
+{
+private: 
+   	unordered_map<string,Symbol> table;    
+   	SymTable * prev;   
+
+public:
+	SymTable();
+	SymTable(SymTable * t);
+	
+	bool Insert(string s, Symbol symb);
+	Symbol * Find(string s); 
 };
